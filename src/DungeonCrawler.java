7a0c2a81fd4bf6716bdcf41152e19bda9269e4bd
Uczen <michal.wojtowicz@tm1.edu.pl>
2024-05-17 09:36:25 +0200

    Commit

--- src/DungeonCrawler.java
+++ src/DungeonCrawler.java
@@ -6,12 +6,22 @@ public class DungeonCrawler {
         start.fill(1,1,15,15, "X");
         start.fill(2,2,14,14, "O");
         start.fill(6,15,10,15, "O");
+        start.exits[1] = true;
 
+        Room corridorLR = new Room();
+        corridorLR.fill(5,1,11,15, "X");
+        corridorLR.fill(6,1,10,15, "O");
+        corridorLR.exits[1] = true;
+        corridorLR.exits[0] = true;
+
+        start.rooms[1] = corridorLR;
+        corridorLR.rooms[0] = start;
 
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Choose your character: ");
-        String character = String.valueOf(reader.nextInt());
+        String character = reader.nextLine();
+
         if(character.length() > 1){
             System.out.println("incorrect character: ");
             return;
@@ -21,6 +31,67 @@ public class DungeonCrawler {
         player.y = 8;
         player.appearance = character;
 
-        start.writeRoom(player.x, player.y);
+
+        boolean working = true;
+        String nextMove;
+        System.out.println("Movement instructions: ");
+        System.out.println("W - Up");
+        System.out.println("S - Down");
+        System.out.println("A - Left");
+        System.out.println("D - Rigth");
+
+        Room currentRoom = start;
+        currentRoom.writeRoom(player.x, player.y, character);
+        while(working){
+            nextMove = "";
+            try {
+                nextMove = reader.nextLine();
+                if(!nextMove.equals("w") && !nextMove.equals("s") && !nextMove.equals("a") && !nextMove.equals("d")){
+                    working = false;
+                }
+            }catch (Exception e){
+                working = false;
+            }
+
+            if(nextMove.equals("w")){
+                if(player.x + 1 > 15){
+                    currentRoom = currentRoom.rooms[2];
+                    player.x = 0;
+                }
+                if(!currentRoom.roomTexture[player.x-2][player.y-1].equals("X")){
+                    player.x--;
+                }
+            }
+            else if(nextMove.equals("s")){
+                if(player.x - 1 < 1){
+                    currentRoom = currentRoom.rooms[3];
+                    player.y = 16;
+                }
+                if(!currentRoom.roomTexture[player.x][player.y-1].equals("X")){
+                    player.x++;
+                }
+            }
+            else if(nextMove.equals("a")){
+                if(player.y - 1 < 1){
+                    currentRoom = currentRoom.rooms[0];
+                    player.y = 16;
+                }
+                if(!currentRoom.roomTexture[player.x-1][player.y-2].equals("X")){
+                    player.y--;
+                }
+            }
+            else if(nextMove.equals("d")){
+                if(player.y + 1 > 15){
+                    currentRoom = currentRoom.rooms[1];
+                    player.y = 0;
+                }
+                if(!currentRoom.roomTexture[player.x-1][player.y].equals("X")){
+                    player.y++;
+                }
+            }
+            currentRoom.writeRoom(player.x, player.y, character);
+            System.out.println(player.x+", "+ player.y);
+        }
+
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -16,11 +16,9 @@ public class Room {
             {"0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"},
             {"0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"},};
 
-    boolean leftExit = false;
-    boolean rightExit = false;
-    boolean upExit = false;
-    boolean downExit = false;
-
+    boolean[] exits = {false, false, false, false};
+    //                 left   right  up     down
+    Room[] rooms =     {null, null, null, null};
     void fill(int leftUpX, int leftUpY, int rightDownX, int rightDownY, String material){
         for(int x = leftUpX; x <= rightDownX; x++){
             for(int y = leftUpY; y <= rightDownY; y++){
@@ -29,14 +27,17 @@ public class Room {
         }
     }
 
-    void writeRoom(int playerX, int playerY){
+    void writeRoom(int playerX, int playerY, String player){
         for(int x = 0; x < 15; x++){
             for(int y = 0; y < 15; y++){
                 if(x == playerX - 1 && y == playerY - 1){
-                    System.out.print("\u001B[34m"+roomTexture[x][y]+"\u001B[0m");
+                    System.out.print("\u001B[35m"+player+"\u001B[0m");
                 }
                 else if(roomTexture[x][y] == "X"){
                     System.out.print("\u001B[31m"+roomTexture[x][y]+"\u001B[0m");
+                }
+                else if(roomTexture[x][y] == "0"){
+                    System.out.print("\u001B[34m"+roomTexture[x][y]+"\u001B[0m");
                 }
                 else{
                     System.out.print(roomTexture[x][y]);
@@ -45,5 +46,6 @@ public class Room {
             }
             System.out.println("");
         }
+        System.out.println("");
     }
 }
